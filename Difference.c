a8d4ab6cee107517225a72372cae06ad96c7c44b
MD. ALIF HOSSAIN ROCKY <59171054+ROCKHOSSAIN@users.noreply.github.com>
2025-11-28 22:02:37 +0600

    Delete Difference.c

--- Difference.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    long long A, B, C, D;              // Change to long long
-    scanf("%lld %lld %lld %lld", &A, &B, &C, &D);   
-    long long X = (A * B) - (C * D);   // Use long long for calculation
-    printf("Difference = %lld\n", X);  // Use %lld for output
-    return 0; 
-}
