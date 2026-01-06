817665e08f1754379ac748795cd761f1d9b90b87
MD. ALIF HOSSAIN ROCKY <59171054+ROCKHOSSAIN@users.noreply.github.com>
2026-01-06 14:17:30 +0600

    Add files via upload

---
+++ C Programming Problems/Function/N_Shift_Zeros.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void fun(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==0){
+                int temp = arr[i];
+                 arr[i]=arr[j];
+                arr[j]=temp;
+                
+            }
+    }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }  
+    }
+int main(){
+       int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    fun(arr,n);
+    
+
+    return 0;
+}
